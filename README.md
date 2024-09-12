1eaf2283971ab779ee864e9c068ee15a98e4eb80
whs31 <40-ryazancev_dl@users.noreply.gitlab.radar076.ru>
2024-09-12 22:16:25 +0300

    Readme

---
+++ README.md
@@ -0,0 +1,20 @@
+[![Build](https://github.com/whs31/renga-api-rs/actions/workflows/build.yml/badge.svg)](https://github.com/whs31/renga-api-rs/actions/workflows/build.yml)[![Docs](https://github.com/whs31/renga-api-rs/actions/workflows/docs.yml/badge.svg)](https://github.com/whs31/renga-api-rs/actions/workflows/docs.yml)
+
+This crate provides (*incomplete*) Rust bindings for the [Renga](https://www.rengabim.com) COM API.
+
+> [!NOTE]
+> Only Windows is supported at the moment.
+
+### Logging
+This crate uses `log` crate interface to log messages.
+You must provide your own logger implementation in order to see logs.
+
+See [log crate](https://crates.io/crates/log) for more details.
+
+### Compatibility
+Rust version at least **1.65** is required.
+This crate is compatible with Renga **8.0.0** and higher.
+
+### Links
+See [Official documentation](https://help.rengabim.com/) for more details about Renga or 
+[Crate documentation](https://whs31.github.io/renga-api-rs/) for more details about this crate.
--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,22 @@
-/// This crate provides (incomplete) bindings for the [Renga](https://www.rengabim.com) COM API.
-/// 
-/// Only Windows is supported at the moment.
-/// 
-/// See [official documentation](https://help.rengabim.com/) for more details.
-/// 
-/// # Logging
-/// 
-/// This crate uses `log` crate interface to log messages.
-/// You must provide your own logger implementation in order to see logs.
-/// 
-/// See [log](https://crates.io/crates/log) for more details.
-/// 
-/// # Compatibility
-/// 
-/// Rust version at least **1.65** is required.
-/// This crate is compatible with Renga 8.0.0 and higher.
-/// 
-/// Todo: examples
+//! This crate provides (*incomplete*) Rust bindings for the [Renga](https://www.rengabim.com) COM API.
+//! 
+//! Only Windows is supported at the moment.
+//! 
+//! See [Official documentation](https://help.rengabim.com/) for more details.
+//! 
+//! # Logging
+//! 
+//! This crate uses `log` crate interface to log messages.
+//! You must provide your own logger implementation in order to see logs.
+//! 
+//! See [log crate](https://crates.io/crates/log) for more details.
+//! 
+//! # Compatibility
+//! 
+//! Rust version at least **1.65** is required.
+//! This crate is compatible with Renga **8.0.0** and higher.
+//! 
+//! Todo: examples
 
 #[macro_use]
 extern crate guid;
